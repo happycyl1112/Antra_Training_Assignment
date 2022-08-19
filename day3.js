9401f57cc20890a6f6892d7a5f109a779fb5938b
happycyl1112 <88967204+happycyl1112@users.noreply.github.com>
2022-08-19 12:19:16 -0700

    Update day3.js

--- day3.js
+++ day3.js
@@ -113,24 +113,10 @@ function isPrime(n) {
 //console.log(isPrime(15));
 
 //9. Write a JavaScript function which accepts an argument and returns the type.
-function detect_data_type(value)
-{
-var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
-    
-if (typeof value === "object" || typeof value === "function") 
-    {
-     for (x = 0, len = dtypes.length; x < len; x++) 
-     {
-            if (value instanceof dtypes[x])
-            {
-                return dtypes[x];
-            }
-      }
-    }
-    
-    return typeof value;
-}
-//console.log(detect_data_type(12));
+function findType(data) {
+    return typeof data;
+  }
+
 //console.log(detect_data_type('w3resource'));
 //console.log(detect_data_type(false));
 
